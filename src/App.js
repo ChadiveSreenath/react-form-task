16f108412e08e7705e8c88177355a327d9ab1b3f
Chadivesreenath <sreenath.chadive@gmail.com>
2023-05-06 13:58:35 +0530

    implemented task

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AirTableForm from './Components/Form';
+
+
+function App() {
+
+  return (
+    <div>
+      <AirTableForm />
+    </div>
+  );
+}
+
+export default App;
